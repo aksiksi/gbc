233e2330a2e7e934c4f94cfcc05aba4e02ac1aa6
Assil Ksiksi <cyph0nik@gmail.com>
2020-12-24 20:36:11 +0000

    Move execute() to cpu module

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,8 +1,8 @@
 use std::convert::TryInto;
 
-use crate::instructions::Instruction;
-use crate::memory::{MemoryBus, MemoryRange};
-use crate::registers::{Flags, RegisterFile};
+use crate::instructions::{Arg, Cond, Instruction};
+use crate::memory::{MemoryBus, MemoryRead, MemoryRange, MemoryWrite};
+use crate::registers::{Flag, Flags, Reg8, Reg16, RegisterFile};
 
 #[derive(Debug)]
 pub struct Cpu {
@@ -35,7 +35,7 @@ impl Cpu {
         dbg!(inst);
 
         // Execute the instruction on this CPU
-        inst.execute(self);
+        self.execute(inst);
 
         if pc == self.registers.PC {
             // If we did not execute a jump, proceed to next instruction as usual
@@ -43,6 +43,161 @@ impl Cpu {
             cycles.not_taken()
         } else {
             cycles.taken()
+        }
+    }
+
+    /// Execute a single instruction on this CPU
+    pub fn execute(&mut self, instruction: Instruction) {
+        use Instruction::*;
+
+        let flags = &mut self.flags;
+        let memory = &mut self.memory;
+        let regs = &mut self.registers;
+
+        let pc = &mut regs.PC;
+
+        match instruction {
+            Nop => (),
+            Ld(dst, src) => {
+                match (dst, src) {
+                    (Arg::Reg16(dst), Arg::Imm16(src)) => {
+                        regs.write_u16(dst, src);
+                    }
+                    (Arg::Reg8(dst), Arg::Imm8(src)) => {
+                        regs.write_u8(dst, src);
+                    }
+                    _ => panic!("Unexpected dst and src: {:?}, {:?}", dst, src),
+                }
+            }
+            LdhA(src) => {
+                let value = memory.read(0xFF00 + src as u16);
+                regs.write_u8(Reg8::A, value);
+            }
+            LdhMemImmA(dst) => {
+                let a = regs.read_u8(Reg8::A);
+                memory.write(0xFF00 + dst as u16, a);
+            }
+            Xor(src) => {
+                let a = regs.read_u8(Reg8::A);
+
+                let result = match src {
+                    Arg::Reg8(src) => {
+                        let curr = regs.read_u8(src);
+                        a ^ curr
+                    }
+                    Arg::Imm8(src) => {
+                        a ^ src
+                    }
+                    Arg::MemHl => {
+                        let addr = regs.read_u16(Reg16::HL);
+                        let curr = memory.read(addr);
+                        a ^ curr
+                    }
+                    _ => panic!("Unexpected src: {:?}", src),
+                };
+
+                regs.write_u8(Reg8::A, result);
+
+                // Flags
+                if result == 0 {
+                    flags.set(Flag::Zero);
+                }
+
+                flags.clear(Flag::Subtract);
+                flags.clear(Flag::Carry);
+                flags.clear(Flag::HalfCarry);
+            }
+            Inc(dst) => {
+                match dst {
+                    Arg::Reg8(dst) => {
+                        let curr = regs.read_u8(dst);
+                        regs.write_u8(dst, curr.wrapping_add(1));
+                    }
+                    Arg::Reg16(dst) => {
+                        let curr = regs.read_u16(dst);
+                        regs.write_u16(dst, curr.wrapping_add(1));
+                    }
+                    Arg::MemHl => {
+                        let addr = regs.read_u16(Reg16::HL);
+                        let curr = memory.read(addr);
+                        memory.write(addr, curr.wrapping_add(1));
+                    }
+                    _ => panic!("Unexpected dst: {:?}", dst),
+                }
+            }
+            Dec(dst) => {
+                // TODO: Flags
+                match dst {
+                    Arg::Reg8(dst) => {
+                        let curr = regs.read_u8(dst);
+                        regs.write_u8(dst, curr.wrapping_sub(1));
+                    }
+                    Arg::Reg16(dst) => {
+                        let curr = regs.read_u16(dst);
+                        regs.write_u16(dst, curr.wrapping_sub(1));
+                    }
+                    Arg::MemHl => {
+                        let addr = regs.read_u16(Reg16::HL);
+                        let curr = memory.read(addr);
+                        memory.write(addr, curr.wrapping_sub(1));
+                    }
+                    _ => panic!("Unexpected dst: {:?}", dst),
+                }
+            }
+            Cp(src) => {
+                let a = regs.read_u8(Reg8::A);
+
+                let other = match src {
+                    Arg::Reg8(src) => {
+                        regs.read_u8(src)
+                    }
+                    Arg::Imm8(src) => {
+                        src
+                    }
+                    Arg::MemHl => {
+                        let addr = regs.read_u16(Reg16::HL);
+                        memory.read(addr)
+                    }
+                    _ => panic!("Unexpected src: {:?}", src),
+                };
+
+                // Flags
+                if other == a {
+                    flags.set(Flag::Zero);
+                } else if other > a {
+                    flags.set(Flag::Carry);
+                }
+            }
+            Jp(addr, cond) => {
+                let ok = match cond {
+                    Cond::None => true,
+                    Cond::NotZero if !flags.is_zero() => true,
+                    Cond::Zero if flags.is_zero() => true,
+                    Cond::NotCarry if !flags.is_carry() => true,
+                    Cond::Carry if flags.is_carry() => true,
+                    _ => false,
+                };
+
+                if ok {
+                    *pc = addr;
+                }
+            }
+            Jr(offset, cond) => {
+                let ok = match cond {
+                    Cond::None => true,
+                    Cond::NotZero if !flags.is_zero() => true,
+                    Cond::Zero if flags.is_zero() => true,
+                    Cond::NotCarry if !flags.is_carry() => true,
+                    Cond::Carry if flags.is_carry() => true,
+                    _ => false,
+                };
+
+                if ok {
+                    // Numeric casts from signed to unsigned will sign extend
+                    *pc = pc.wrapping_add(offset as u16);
+                }
+            }
+            other => panic!("Cannot execute instruction: {:?}", other),
         }
     }
 
