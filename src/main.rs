676e753ec581b6eddd013c8d610c341f18ec2486
Assil Ksiksi <cyph0nik@gmail.com>
2020-12-28 01:28:05 -0500

    Working example using SDL

--- src/main.rs
+++ src/main.rs
@@ -13,22 +13,34 @@ fn gui() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    // Setup an SDL2 Window
+    let mut window = video_subsystem.window("gbc", 800, 600)
         .position_centered()
+        .allow_highdpi()
         .build()
         .unwrap();
 
+    // Convert the Window into a Canvas
+    // This is what we will use to render content in the Window
     let mut canvas = window.into_canvas().build().unwrap();
+
+    // Get a handle to the Canvas texture creator
     let texture_creator = canvas.texture_creator();
 
+    // Create a Texture
+    // We write raw pixel data here and copy it to the Canvas for rendering
+    let mut texture = texture_creator.create_texture(None,
+                                                     TextureAccess::Target,
+                                                     800,
+                                                     600).unwrap();
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
-
-    let mut event_pump = sdl_context.event_pump().unwrap();
     let mut i = 0;
     let mut j = 0;
+
+    let gameboy = Gameboy::init("samples/pokemon_gold.gbc").unwrap();
+
+    // Start the event loop
+    let mut event_pump = sdl_context.event_pump().unwrap();
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -40,34 +52,38 @@ fn gui() {
             }
         }
 
-        // Draw a 100x100 rect on a 800x600 texture at a varying position
-        canvas.clear();
-        let mut texture = texture_creator.create_texture(None,
-                                                         TextureAccess::Streaming,
-                                                         800,
-                                                         600).unwrap();
-        let rect = Rect::new(i * 100, i * 100, 100, 100);
-        let mut pixels = Vec::new();
-        let c = Color::MAGENTA.rgba();
-        for i in 0..100 {
-            for j in 0..100 {
-                pixels.extend(&[c.0, c.1, c.2, c.3]);
+        // Remember the texture we created above? That is basically a buffer in VRAM
+        // With the following, we are setting that texture as a render target for
+        // our main canvas. This allows us to use regular canvas drawing functions -
+        // e.g., rect, point - to update the GPU texture buffer.
+        //
+        // Once this closure ends, the canvas target is reset back for us.
+        //
+        // Helpful C example: https://wiki.libsdl.org/SDL_CreateTexture
+        canvas.with_texture_canvas(&mut texture, |canvas| {
+            let rect = Rect::new(i * 100, i * 100, 100, 100);
+            canvas.set_draw_color(Color::YELLOW);
+            canvas.clear();
+            canvas.draw_rect(rect).unwrap();
+            canvas.set_draw_color(Color::MAGENTA);
+            canvas.fill_rect(Some(rect)).unwrap();
+
+            for i in 300..500 {
+                canvas.draw_point((i, 100)).unwrap();
             }
-        }
+        }).unwrap();
 
-        texture.update(Some(rect), &pixels, 100 * 4).unwrap();
-
+        // Once we've completed our texture operations, we need to copy the texture
+        // back to the canvas and present it.
         canvas.copy(&texture, None, None).unwrap();
         canvas.present();
 
         i = (i + 1) % 8;
         j = (j + 1) % 6;
 
-
         // The rest of the game loop goes here...
 
-        // canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
 
