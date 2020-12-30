fb4f41795c141ef6f019fb13b0425c1bb5681edc
Assil Ksiksi <cyph0nik@gmail.com>
2020-12-29 23:52:29 -0500

    Split out LCD registers

--- src/gbc/ppu.rs
+++ src/gbc/ppu.rs
@@ -1,3 +1,50 @@
+//! Gameboy PPU and LCD handling
+//!
+//! # Overview
+//!
+//! ## Background
+//!
+//! The Gameboy screen buffer holds 256x256 pixels, or 32x32 *tiles*.
+//! However, the LCD only displays 160x144 pixels at a time.
+//!
+//! `SCROLLX` and `SCROLLY` registers contain the location of the background at the
+//! upper left of the screen (i.e., it is scrollable). Note that the background
+//! wraps around the screen edges.
+//!
+//! The *Background Tile Map* contains 32 rows of 32 bytes each. Each byte
+//! maps to a tile number. Tiles are stored in the *Tile Data Table* in VRAM
+//! in one of these two regions:
+//!
+//! * 0x8000-0x8FFF (tile number = 0 to 255)
+//! * 0x8800-0x97FF (tile number = -128 to 127, 0th tile at 0x9000)
+//!
+//! The region is set/modified using the LCDC register.
+//!
+//! *BG Display Data*, or the actual content of the background (256 x 256 pixels),
+//! is stored at either:
+//!
+//! * 0x9800-0x9BFF
+//! * 0x9C00-0x9FFF
+//!
+//! The region is set using bit 3 of the LCDC register.
+//!
+//! The aforementioned scroll registers determine which area of the BG is displayed
+//! on the 160x144 LCD.
+//!
+//! ## Window
+//!
+//! WX and WY control where the window is displayed on the LCD. Note that the window
+//! does not wrap and is not scrollable.
+//!
+//! ## LCD
+//!
+//! Each row of 160 pixels takes 108.7 us to display. If you multiply that by 144 rows,
+//! the total display time is ~15.66 ms.
+//!
+//! Once the frame is displayed, the VBLANK period lasts 10 lines, which maps to ~1.09 ms.
+//! This is when VRAM data can be accessed.
+//!
+//! The combination of these two periods nets us ~60 fps.
 use crate::memory::{MemoryRead, MemoryWrite};
 
 pub enum Vram {
@@ -5,13 +52,13 @@ pub enum Vram {
         /// Static bank, 8K
         ///
         /// Non-CGB mode
-        data: [u8; Self::BANK_SIZE],
+        data: Box<[u8; Self::BANK_SIZE]>,
     },
     Banked {
         /// Two static banks, 8K each
         ///
         /// CGB mode
-        data: [u8; Self::BANK_SIZE * 2],
+        data: Box<[u8; Self::BANK_SIZE * 2]>,
         active_bank: u8,
     },
 }
@@ -25,12 +72,12 @@ impl Vram {
     pub fn new(cgb: bool) -> Self {
         if cgb {
             Self::Banked {
-                data: [0u8; Self::BANK_SIZE * 2],
+                data: Box::new([0u8; Self::BANK_SIZE * 2]),
                 active_bank: 0,
             }
         } else {
             Self::Unbanked {
-                data: [0u8; Self::BANK_SIZE],
+                data: Box::new([0u8; Self::BANK_SIZE]),
             }
         }
     }
@@ -105,19 +152,67 @@ pub struct Ppu {
     vram: Vram,
 
     /// OAM (0xFE00-0xFE9F)
-    oam: [u8; 0x9F],
+    ///
+    /// 40 objects can be loaded into this RAM. Each 4-byte object
+    /// consists of:
+    ///
+    /// 1. y-coordinate (8 bits)
+    /// 2. x-coordinate (8 bits)
+    /// 3. CHR code (8 bits)
+    /// 4. BG and OBJ display priority (1 bit)
+    /// 5. Vertical flip (1 bit)
+    /// 6. Horizontal flip (1 bit)
+    /// 6. DMG mode palette (1 bit)
+    /// 7. Character blank (1 bit)
+    /// 8. Color palette (3 bits)
+    oam: [u8; 160],
 
-    /// LCD registers (0xFF40 - 0xFF4B)
-    // TODO
-    lcd: [u8; 12],
+    /// LCD control register (0xFF40)
+    lcdc: u8,
+
+    /// LCD status register (0xFF41)
+    stat: u8,
+
+    /// Background position registers (0xFF42, 0xFF43)
+    ///
+    /// Range: 0x00-0xFF (256 x 256 pixels)
+    scy: u8,
+    scx: u8,
+
+    /// LCD line register: 0xFF44
+    ///
+    /// Note: LYC at 0xFF45 maps to this same value.
+    ly: u8,
+
+    /// Window position registers (0xFF4A, 0xFF4B)
+    ///
+    /// Ranges: 0 <= WY <= 143 and 7 <= WX <= 166
+    wy: u8,
+    wx: u8,
+
+    /// Color data registers (0xFF68-0xFF6B)
+    bcps: u8,
+    bcpd: u8,
+    ocps: u8,
+    ocpd: u8,
 }
 
 impl Ppu {
     pub fn new(cgb: bool) -> Self {
         Self {
             vram: Vram::new(cgb),
-            lcd: [0u8; 12],
-            oam: [0u8; 0x9F],
+            oam: [0u8; 160],
+            lcdc: 0,
+            stat: 0,
+            scy: 0,
+            scx: 0,
+            ly: 0,
+            wy: 0,
+            wx: 0,
+            bcps: 0,
+            bcpd: 0,
+            ocps: 0,
+            ocpd: 0,
         }
     }
 
@@ -139,10 +234,17 @@ impl MemoryRead<u16, u8> for Ppu {
                 let idx = (addr as usize) - 0xFE00;
                 self.oam[idx]
             }
-            0xFF40..=0xFF4B => {
-                let idx = (addr as usize) - 0xFF40;
-                self.lcd[idx]
-            }
+            0xFF40 => self.lcdc,
+            0xFF41 => self.stat,
+            0xFF42 => self.scy,
+            0xFF43 => self.scx,
+            0xFF44 | 0xFF45 => self.ly,
+            0xFF4A => self.wy,
+            0xFF4B => self.wx,
+            0xFF68 => self.bcps,
+            0xFF69 => self.bcpd,
+            0xFF6A => self.ocps,
+            0xFF6B => self.ocpd,
             _ => panic!("Unexpected read from addr {}", addr),
         }
     }
@@ -157,10 +259,17 @@ impl MemoryWrite<u16, u8> for Ppu {
                 let idx = (addr as usize) - 0xFE00;
                 self.oam[idx] = value;
             }
-            0xFF40..=0xFF4B => {
-                let idx = (addr as usize) - 0xFF40;
-                self.lcd[idx] = value;
-            }
+            0xFF40 => self.lcdc = value,
+            0xFF41 => self.stat = value,
+            0xFF42 => self.scy = value,
+            0xFF43 => self.scx = value,
+            0xFF44 | 0xFF45 => self.ly = value,
+            0xFF4A => self.wy = value,
+            0xFF4B => self.wx = value,
+            0xFF68 => self.bcps = value,
+            0xFF69 => self.bcpd = value,
+            0xFF6A => self.ocps = value,
+            0xFF6B => self.ocpd = value,
             _ => panic!("Unexpected write to addr {} value {}", addr, value),
         }
     }
