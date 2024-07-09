4b1b5cf64e398caef0960a1fe1a34bb3c3974b35
aksiksi <aksiksi@users.noreply.github.com>
2024-07-09 15:28:46 +0000

    deploy: 7e60034360a2e59e90e8c3155877cd0288d3fd17

--- bootstrap.js
+++ bootstrap.js
@@ -195,7 +195,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/gbcemu_bg.wasm":"7022ea7c0fa83914242b"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/gbcemu_bg.wasm":"3892f3ae5ba37d227458"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
